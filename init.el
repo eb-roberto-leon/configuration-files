0f45964dacebf00196e46c5e61989ae1527b367d
Roberto Leon <rleondev@gmail.com>
2019-11-12 16:49:53 +0100

    Update init.el

--- init.el
+++ init.el
@@ -5,7 +5,6 @@
 (global-linum-mode t)
 (global-hl-line-mode t)
 
-
 ;; Package configs
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -48,31 +47,6 @@
   (setq which-key-prefix-prefix "+")
   :config
   (which-key-mode))
-
-;; Custom keybinding
-; (use-package general
-;   :ensure t
-;   :config (general-define-key
-;   :states '(normal visual insert emacs)
-;   :prefix "SPC"
-;   :non-normal-prefix "M-SPC"
-;   ;; "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
-;   "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
-;   "SPC" '(helm-M-x :which-key "M-x")
-;   "pf"  '(helm-find-file :which-key "find files")
-;   ;; Buffers
-;   "bb"  '(helm-buffers-list :which-key "buffers list")
-;   ;; Window
-;   "wl"  '(windmove-right :which-key "move right")
-;   "wh"  '(windmove-left :which-key "move left")
-;   "wk"  '(windmove-up :which-key "move up")
-;   "wj"  '(windmove-down :which-key "move bottom")
-;   "w/"  '(split-window-right :which-key "split right")
-;   "w-"  '(split-window-below :which-key "split bottom")
-;   "wx"  '(delete-window :which-key "delete window")
-;   ;; Others
-;   "at"  '(ansi-term :which-key "open terminal")
-; ))
 
 ;; Fancy titlebar for MacOS
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
