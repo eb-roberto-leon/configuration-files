99276126c64698432503aa3f52a428918c578cb6
Roberto Leon <rleondev@gmail.com>
2019-11-12 16:55:04 +0100

    Update init.el

--- init.el
+++ init.el
@@ -47,10 +47,3 @@
   (setq which-key-prefix-prefix "+")
   :config
   (which-key-mode))
-
-;; Fancy titlebar for MacOS
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon  nil)
-(setq frame-title-format nil)
-
